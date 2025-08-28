fc4cb7d25f48044b4408da7293d10151d7759edb
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-08-28 18:55:42 +0530

    Integrate GSAP and Tailwind CSS into the project, updating dependencies in package.json and package-lock.json. Configure Vite to use Tailwind CSS and add a basic GSAP animation in App.jsx. Include Tailwind CSS in the index.css file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
 import React from 'react'
 
 const App = () => {
   return (
     <div>
-      
+      <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
     </div>
   )
 }
